8e09efdb2255f342d31775e4a083f273af4b5707
small-dragon <smalldragon852@gmail.com>
2017-10-31 17:39:32 +0800

    第二次敲zepto源码

--- fe-test/zepto/my-zepto.js
+++ fe-test/zepto/my-zepto.js
@@ -21,7 +21,7 @@ var Zepto = (function() {
       'td': tableRow, 'th': tableRow,
       '*': document.createElement('div')
     },
-    simpeSelectRE = /^[\w-]*$/,
+    simpeSelectorRE = /^[\w-]*$/,
     class2type = {},
     toString = class2type.toString,
     zepto = {}，
@@ -145,10 +145,243 @@ var Zepto = (function() {
       if (isPlainObject(properties)) {
         nodes = $(dom)
         $.each(properties, function(key, value) {
-          if (methodAttributes.indexOf(key) > -1) nodes[key][value]
+          if (methodAttributes.indexOf(key) > -1) nodes[key](value)
           else nodes.attr(key, value)
         })
       }
       return dom
+    }
+
+    zepto.Z = function(dom, selector) {
+      return new Z(dom, selector)
+    }
+
+    zepto.isZ = function(object) {
+      return object instanceof zepto.Z
+    }
+
+    zepto.init = function(selector, context) {
+      var dom
+      if (!selector) return zepto.Z()
+      else if (typeof selector == 'string') {
+        selector = selector.trim()
+        if (selector[0] == '<' && fragmentRE.test(selector))
+          dom = zepto.fragment(selector, RegExp.$1, context) selector = null
+        else if (context !== undefined) return $(context).find(selector)
+        else dom = zepto.qsa(document, selector)
+      }
+      else if (isFunction(selector)) return $(document).ready(selector)
+      else if (zepto.isZ(selector)) return selector
+      else {
+        if (isArray(selector)) dom = compact(selector)
+        else if (isObject(selector))
+          dom = [selector], selector = null
+        else if (fragmentRE.test(selector))
+          dom = zepto.fragment(selector.trim(), RegExp.$1, context) selecor = null
+        else if (context !== undefined) return $(context).find(selector)
+        else dom = zepto.qsa(document, selector)
+      }
+      return zepto.Z(dom, selector)
+    }
+
+    $ = function(selector, context) {
+      return zepto.init(selector, context)
+    }
+
+    function extent(target, source, deep) {
+      for (key in source)
+        if (deep && (isPlainObject(source[key]) || isArray(source[key]))) {
+          if (isPlainObject(source[key]) && !isPlainObject(target[key]))
+            target[key] = {}
+          if (isArray(source[key]) && !isArray(target[key]))
+            target[key] = []
+          extent(target[key], source[key], deep)
+        }
+        else if (source[key] !== undefined) target[key] = source[key]
+    }
+
+    $.extend = function(target) {
+      var deep, args = slice.call(argument, 1)
+      if (typeof target == 'boolean') {
+        deep = target
+        target = args.shift()
+      }
+      args.forEach(function(arg){ extend(target, arg, deep) })
+      return target
+    }
+
+    zepto.qsa = function(element, selector) {
+      var found,
+          maybeID = selector[0] == '#',
+          maybeClass = !maybeID && selector[0] == '.',
+          nameOnly = maybeID || maybeClass ? selector.slice(1) : selector,
+          isSimple = simpleSelectorRE.test(nameOnly)
+      return (element.getElementById && isSimple && maybeID) ? 
+        ( ( found = element.getElementById(nameOnly)) ? [found] : []) : 
+        slice.call(
+          isSimple && !maybeID && element.getElementsByClassName ?
+            maybeClass ? element.getElementsByClassName(nameOnly) :
+            element.getElementsByTagName(selector) :
+            element.querySelectorAll(selector)
+        )
+    }
+
+    function filterred(nodes, selector) {
+      return selector == null ? $(nodes) : $(nodes).filter(selector)
+    }
+
+    $.contains = document.documentElement.contains ? 
+      function (parent, node) {
+        return parent !== node && parent.contains(node)
+      } :
+      function(parent, node) {
+        while (node && (node = node.parentNode))
+          if (node === parent) return true
+        return false
+      }
+
+    function funcArg(context, arg, idx, payload) {
+      return isFunction(arg) ? arg.call(context, idx, payload) : arg
+    }
+
+    function setAttribute(node, name, value) {
+      value == null ? node.removeAttribute(name) : node.setAttribute(name, value)
+    }
+
+    function className(node, value){
+      var klass = node.className || '',
+          svg = klass && klass.baseVal !== undefined
+
+        if (value === undefined) return svg ? klass.baseVal : klass
+        svg ? (klass.baseVal = value) : (node.className = value)
+    }
+
+    function deserializeValue(value) {
+      try {
+        return value ?
+          value == 'true' ||
+          ( value == 'false' ? false :
+            value == 'null' ? null : 
+            +value + '' == value ? +value :
+            /^[\[\{]/.test(value) ? $.parseJSON(value) :
+            value )
+          : value
+      } catch(e) {
+        return value
+      }
+    }
+
+    $.type = type
+    $.isFunction = isFunction
+    $.isWindow = isWindow
+    $.isArray = isArray
+    $.isPlainObject = isPlainObject
+
+    $.isEmptyObject = function(obj) {
+      var name
+      for (name in obj) return false
+      return true
+    }
+
+    $.isNumeric = function(val) {
+      var num = Number(val), type = typeof val
+      return val != null && type != 'boolean' && 
+        (type != 'string' || val.length) &&
+        !isNaN(num) && isFinite(num) || false
+    }
+
+    $.inArray = function(elem, array, i){
+      return emptyArray.indexOf.call(array, elem, i)
+    }
+
+    $.camelCase = camelize
+    $.trim = function(str) {
+      return str == null ? '' : String.prototype.trim.call(str)
+    }
+
+    $.uuid = 0
+    $.support = { }
+    $.expr = { }
+    $.noop = function() {}
+
+    $.map = function(elements, callback) {
+      var value, values = [], i, key
+      if (likeArray(elements)) 
+        for (i = 0; i < elements.length; i++) {
+          value = callback(elements[i], i)
+          if (value != null) values.push(value)
+        }
+      else 
+        for (key in elements) {
+          value = callback(elements[key], key)
+          if (value != null) values.push(value)
+        }
+      return flatten(values)
+    }
+
+    $.each = function(elements, callback) {
+      var i, key
+      if (likeArray(elements)) {
+        for (i = 0; i < elements.length; i++) 
+          if (callback.call(elements[i], i, elements[i]) === false) return elements
+      } else {
+        for (key in elements)
+          if (callback.call(elements[key], key, elements[key]) === false) return elements
+      }
+
+      return elements
+    }
+
+    $.grep = function(elements, callback){
+      return filter.call(elements, callback)
+    }
+
+    if (window.JSON) $.parseJSON = JSON.parse
+
+    $.each('Boolean Number String Function Array Date RegExp Object Error').split(' '), function(i, name) {
+      class2type[ "[object " + name + "]"] = name.toLowerCase()
+    }
+
+    $.fn = {
+      constructor: zepto.Z,
+      length: 0,
+
+      forEach: emptyArray.forEach,
+      reduce: emptyArray.reduce,
+      push: emptyArray.push,
+      sort: emptyArray.sort,
+      splice: emptyArray.splice,
+      indexOf: emptyArray.indexOf,
+      concat: function() {
+        var i, value, args = []
+        for (i = 0; i < arguments.length; i++) {
+          value = arguments[i]
+          args[i] = zepto.isZ(value) ? value.toArray() : value
+        }
+        return concat.apply(zepto.isZ(this) ? this.toArray() : this, args)
+      },
+
+      map: function(fn) {
+        return $($.map(this, function(el, i){ return fn.call(el, i, el) }))
+      },
+      slice: function() {
+        return $(slice.apply(this, arguments))
+      },
+
+      ready: function(callback) {
+        if (document.readyState === 'complete' ||
+          (document.readyState !== 'loading' && !document.documentElement.doScroll))
+          setTimeout(function(){ callback($) }, 0)
+        else  {
+          var handler = function() {
+            document.removeEventListener('DOMContentLoaded', handler, false)
+            window.removeEventListener('load', handler, false)
+            callback($)
+          }
+          document.addEventListener('DOMContentLoaded', handler, false)
+          window.addEventListener('load', handler, false)
+        }
+        return this
+      }
     }
 })
